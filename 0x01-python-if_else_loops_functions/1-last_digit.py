a8677b4c179352216e37389508650cfaef51324e
jimmythongtran <jimmytranman@gmail.com>
2017-01-03 22:42:43 +0000

    Multiply negative 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if l > 5:
     print("Last digit of {} is {} is and is greater than 5".format(n, l))
 elif l == 0:
     print("Last digit of {} is {} and is 0".format(n, l))
-elif l < 6:
+elif (l < 6 * -1):
     print("Last digit of {} is {} and is less than 6 and not 0".format(n, l))
