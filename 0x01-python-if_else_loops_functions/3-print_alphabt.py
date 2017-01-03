f2e870e0e34a268676189563a182419b6336b928
jimmythongtran <jimmytranman@gmail.com>
2017-01-03 23:45:33 +0000

    Alternative way to 2 too

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) == ord('e') | ord('q'):
-        pass
-    else:
-        print(chr(i), end="")
+for i in "abcdfghijklmnoprstuvwxyz":
+    print("{}".format(i), end="")
