2dae5b9ebc24f6f639750b59b2b3a2117009fed6
jimmythongtran <jimmytranman@gmail.com>
2017-01-03 21:30:37 +0000

    Back to loop, end

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", end="")
-# for i in range(ord('a'), ord('z')+1):
-#    print(chr(i), end="")
+for i in range(ord('a'), ord('z')+1):
+    print(chr(i), end="")
