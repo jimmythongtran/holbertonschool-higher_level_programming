49ba4ff972480836120a1d543879cf839cd8b661
jimmythongtran <jimmytranman@gmail.com>
2017-01-04 04:20:43 +0000

    First draft

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#def fizzbuzz()
+
+i = 1
+while i <= 100:
+    if i % 3 == 0 | i % 5 == 0
+        print("FizzBuzz")
+    elif i % 3 == 0
+        print("Fizz")
+    elif i % 5 == 0
+        print("Buzz")
+    else
+        print(i)
+    if i != 100
+        print(" ")
