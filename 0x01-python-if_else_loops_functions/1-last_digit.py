9d5a5d25941987c4b246926d65fe3c8061e7b412
jimmythongtran <jimmytranman@gmail.com>
2017-01-04 03:59:34 +0000

    Neg output good lines too long

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
-n = random.randint(-10000, 10000)
-l = (n % 10)
-if l > 5:
-    print("Last digit of {} is {} is and is greater than 5".format(n, l))
-elif l == 0:
-    print("Last digit of {} is {} and is 0".format(n, l))
-elif (l < 6 * -1):
-    print("Last digit of {} is {} and is less than 6 and not 0".format(n, l))
+number = random.randint(-10000, 10000)
+if number >= 0:
+    lastDigit = number % 10
+else:
+    lastDigit = ((number * -1) % 10) * -1
+
+if lastDigit > 5:
+    print("Last digit of {} is {} is and is greater than 5".format(number, lastDigit))
+elif lastDigit == 0:
+    print("Last digit of {} is {} and is 0".format(number, lastDigit))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastDigit))
