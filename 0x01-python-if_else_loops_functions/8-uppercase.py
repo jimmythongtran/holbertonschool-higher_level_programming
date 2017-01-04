b8a24cadc6b447e586b670f9ab2e6ae429814c11
jimmythongtran <jimmytranman@gmail.com>
2017-01-04 03:50:19 +0000

    Indentation errors

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+		var = (chr(ord(i) - 32))
+        else:
+		var = (i)
+	print("{}".format(var), end="")
+    print("{}".format(""))
