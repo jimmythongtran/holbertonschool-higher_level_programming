b8f89c5638a86c9a2100ee9fe2ab118923840eba
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-04 06:47:12 +0000

    Fix .vimrc

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for i in str:
         if ord('a') <= ord(i) <= ord('z'):
-		var = (chr(ord(i) - 32))
+            var = (chr(ord(i) - 32))
         else:
-		var = (i)
-	print("{}".format(var), end="")
+            var = (i)
+        print("{}".format(var), end="")
     print("{}".format(""))
