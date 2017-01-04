f590fdd0ae089c0f140bae9dff3007036a4d4b0e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-04 20:30:26 +0000

    Moved print 89 as first cond

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range(1, 9):
-        if i < j:
+    for j in range(1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        elif i < j:
             print("{:d}{:d}, ".format(i, j), end="")
-print("{:d}".format(89))
+#print("{:d}".format(89))
 # print("")
