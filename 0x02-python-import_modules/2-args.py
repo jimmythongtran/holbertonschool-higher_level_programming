713c0b61e682692cb72c773204f827f768637280
jimmythongtran <jimmytranman@gmail.com>
2017-01-06 01:20:02 +0000

    Add -1 to exclude filename

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print("{:d} argument.".format(len(sys.argv) -1)) # > 0 argument.
 
-    if len(sys.argv) == 1:
-        print("{:d} argument:".format(len(sys.argv))) # > 1 argument:
+    if len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) -1)) # > 1 argument:
 
-    if len(sys.argv) > 1:
-        print("{:d} arguments:".format(len(sys.argv))) # > 2+ arguments:
+    if len(sys.argv) > 2:
+        print("{:d} arguments:".format(len(sys.argv) -1)) # > 2+ arguments:
 #end=""
 #end="s"
 
