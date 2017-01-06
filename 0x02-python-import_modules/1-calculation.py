1c6fb63b87e8e12a8e15e1d895bff5d68e02b3b0
jimmythongtran <jimmytranman@gmail.com>
2017-01-06 00:34:00 +0000

    Moved vars under import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-from calculator_1 import add, sub, mul, div
 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
