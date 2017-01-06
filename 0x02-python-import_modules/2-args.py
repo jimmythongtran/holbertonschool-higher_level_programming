5b4c425cd43434cc9f12cca04e8a6fc9af96ebc5
jimmythongtran <jimmytranman@gmail.com>
2017-01-06 01:44:19 +0000

    Argument conditionals work

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,19 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("{:d} argument.".format(len(sys.argv) -1)) # > 0 argument.
 
+# bake in the one-line-per-argument"
     if len(sys.argv) == 2:
-        print("{:d} argument:".format(len(sys.argv) -1)) # > 1 argument:
+        print("{:d} argument: {}".format(len(sys.argv) -1, (sys.argv)) # > 1 argument:
 
     if len(sys.argv) > 2:
         print("{:d} arguments:".format(len(sys.argv) -1)) # > 2+ arguments:
+
+#while loop to print out argumentInput numberAscending:
+#while loop to print out next argumentInput
+    for i in sys.argv
+        print("{:d}".format(sys.argv)) #argumentInput numberAscending
+	print("{:s}".format(sys.argv)) #argumentInput
+
 #end=""
 #end="s"
 
