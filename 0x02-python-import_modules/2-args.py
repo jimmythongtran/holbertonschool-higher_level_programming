f38463d848319f7745396dfc0d9198bde6cde2fe
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-06 20:43:51 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,16 @@
 if __name__ == "__main__":
     import sys
 # put len into var, cleaner
-    i = len(sys.argv) - 1
-    if i == 0:
-        print("{:d} argument.".format(i))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("{:d} argument.".format(count))
 
-    elif i == 1:
-        print("{:d} argument:".format(i))
+    elif count == 1:
+        print("{:d} argument:".format(count))
 
     else:
-        print("{:d} arguments:".format(i))  # 2+ arguments:
+        print("{:d} arguments:".format(count))  # 2+ arguments:
 
-    if i >= 1:
-        for n in range(0, i):
+    if count >= 1:
+        for n in range(0, count):
             print("{:d}: {}".format(n + 1, sys.argv[n + 1]))  # arg#Asc, argInp
