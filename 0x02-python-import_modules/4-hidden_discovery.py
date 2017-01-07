82da3c7cb9a7fdfe6df976181a41eb99815f2abd
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-07 00:29:17 +0000

    Refactor didn't work

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,3 +5,6 @@ if __name__ == "__main__":
     for a in range(0, size):
         if dir(hidden_4)[a][0:2] != "__":
             print(dir(hidden_4)[a])
+#    for a in dir(hidden_4):
+#        if dir(hidden_4)[a][0:2] != "__":
+#            print(dir(hidden_4)[a])
