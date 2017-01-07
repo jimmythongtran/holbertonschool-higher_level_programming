3cf52619998d9c9555f234015edbfe4f6ce19e87
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-07 00:14:44 +0000

    Moved import down

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
+    import hidden_4
     size = len(dir(hidden_4))
     for a in range(0, size):
         if dir(hidden_4)[a][0:2] != "__":
