999975c5f7bf7243334ea3bdb7c5610c157ac493
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-07 00:10:28 +0000

    Misplaced closing parenthesis

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
-    for a in dir(hidden_4):
-        print (a)    
+    size = len(dir(hidden_4))
+    for a in range(0, size):
+        if dir(hidden_4)[a][0:2] != "__":
+            print(dir(hidden_4)[a])
