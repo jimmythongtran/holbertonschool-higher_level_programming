22f3933d3c4f138701b16cefb8612c9c5b863d88
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-10 23:28:32 +0000

    Have Pseudocode

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for i in range(0, len(my_list)):
+        if i == idx:
+# replace element at idx with a new_element
+            return (my_list[idx])
