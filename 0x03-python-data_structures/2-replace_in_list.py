61bbde40c18b4c1ca737ea37bccc82c1260d976b
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-10 23:41:07 +0000

    Works but not exact output

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,6 @@
 def replace_in_list(my_list, idx, element):
     for i in range(0, len(my_list)):
         if i == idx:
+            my_list[idx] = element
 # replace element at idx with a new_element
             return (my_list[idx])
