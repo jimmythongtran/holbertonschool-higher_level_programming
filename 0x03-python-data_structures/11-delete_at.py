655bc53ef266501253e1778ddf8433c88594729a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-11 22:56:49 +0000

    Not equal to len(my_list)

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if not my_list:
         return my_list
-    if 0 <= idx <= len(my_list):
+    if 0 <= idx < len(my_list):
         del my_list[idx]
     return my_list
 #    for i in my_list[:]:
