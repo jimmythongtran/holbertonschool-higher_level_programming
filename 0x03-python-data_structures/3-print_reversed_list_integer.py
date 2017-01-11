4c3c0492e0ffeee8a49ef9b9ce7564212892a31b
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-11 01:37:05 +0000

    Checking for NULL/NONE

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        return None
     for i in reversed(my_list):
         #  my_list.reverse()
         print("{:d}".format(i))
+#  TODO: You have to handle the list when it == None
