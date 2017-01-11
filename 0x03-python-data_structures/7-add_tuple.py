345e4981b8b23ec6dd92e2a528c2bfda23061d77
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-11 23:24:01 +0000

    Thanks Ian

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    #  first element the addition of the first element of each argument
-    a = tuple_a(0) + tuple_b(0)
-    #  second element the addition of the second element of each argument
-    b = tuple_a(1) + tuple_b(1)
-#    if a or b > 2, use the value 0 for each missing integer
-#    if a or b < 2, use only the first two integers
-    return tuple(a, b)
+    # if user passes in an empty tuple, end up with an index out of range error
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
