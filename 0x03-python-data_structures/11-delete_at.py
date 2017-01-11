67a77c8e743abdd6d98121459416bc88ee182648
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-11 07:43:30 +0000

    if not works, try return move

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@ def delete_at(my_list=[], idx=0):
         return my_list
     if 0 <= idx <= len(my_list):
         del my_list[idx]
-        return my_list
+    return my_list
 #    for i in my_list[:]:
 #        my_list.remove(i)
 #    return (my_list)
