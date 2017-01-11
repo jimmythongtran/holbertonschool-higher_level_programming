cfb4d708e2f67089d33874516ae7f00897861564
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-11 00:21:42 +0000

    [:] somehow

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+#  TODO: Return a copy of the list
 def new_in_list(my_list, idx, element):
     for i in range(0, len(my_list)):
         if i == idx:
             my_list[idx] = element  # replace element at idx with a new_element
-    return (my_list)
+            my_list[:] #  returns a shallow copy
+    return (my_list[:])
