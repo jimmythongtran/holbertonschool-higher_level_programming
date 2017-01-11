cba33a880def60792815d8f83ed3deee23a3e1d2
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-11 07:35:25 +0000

    Realign returnwq

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if 0 <= idx <= len(my_list):
         del my_list[idx]
-        return my_list
+    return my_list
 #    for i in my_list[:]:
 #        my_list.remove(i)
 #    return (my_list)
