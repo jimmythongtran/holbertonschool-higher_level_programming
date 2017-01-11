7f75f0138cbde133a36db3b7e1620c6d0b299c7b
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-11 00:31:17 +0000

    Sliced list to copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 #  TODO: Return a copy of the list
 def new_in_list(my_list, idx, element):
     for i in range(0, len(my_list)):
+        my_list = my_list[:]  # returns a shallow copy
         if i == idx:
             my_list[idx] = element  # replace element at idx with a new_element
-            my_list[:] #  returns a shallow copy
-    return (my_list[:])
+    return (my_list)
