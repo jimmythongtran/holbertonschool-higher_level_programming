38e36a8cacf371878b29fe59ad1b5934143fd87d
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-11 06:25:03 +0000

    None test works and pep

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-#    if sentence is None:
-#        return None
+    #    if sentence is None:
+    #        return None
     if len(sentence) > 0:
         return tuple([len(sentence), sentence[0]])
     else:
