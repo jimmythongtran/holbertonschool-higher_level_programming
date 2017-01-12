a9d99742eb0aeb640400e9acf081fb53bc7ce298
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-12 23:07:07 +0000

    Add None

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     for unique in my_list:
+        if my_list is None:
+            return None
         unique = set(my_list)
         return(sum(unique))
