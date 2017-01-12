efff36724ad5faa20c5bb621e09175113437d1a4
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-12 23:10:22 +0000

    Add blank dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None:
+    if my_dict == {} or my_dict is None:
         return None
     top = max(my_dict, key=my_dict.get)
     return (top)
