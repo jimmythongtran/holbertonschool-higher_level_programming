81106b68b2793f68fb784319e1482363fd4279f6
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-12 22:45:58 +0000

    Stored in unique

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return {i for i in set_1 and set_2 if i not in set_1 or set_2}
+    unique = []
+    {unique.append(i) for i in set_1 if i not in set_2}
+    {unique.append(i) for i in set_2 if i not in set_1}
+#    return {i for i in set_1 and set_2 if i not in set_1 or set_2}
+    return unique
