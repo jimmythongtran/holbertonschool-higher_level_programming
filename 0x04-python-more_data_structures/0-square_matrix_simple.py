ae1b409d0ea62732cfa89349f79760e2fcd87a72
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-12 18:53:02 +0000

    Sqrt temp, no output

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,6 @@
 def square_matrix(matrix=[]):
     for row in range(0, len(matrix)):
         for i in range(0, len(matrix[row])):
-            i * i
-    return(matrix)
+           i = (i * i)
+           i = (i **.5)
+#    return matrix
