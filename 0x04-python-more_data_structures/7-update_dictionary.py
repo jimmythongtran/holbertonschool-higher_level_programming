ad692afdbde8007341309670159efc887237760e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-13 07:12:19 +0000

    Key values amazing

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
-    for key, value in my_dict:
-        if key in my_dict:
-            value is replaced
-        if key not in my_dict:
-            create value
+    my_dict[key] = value
     return (my_dict)
+#    for key, value in my_dict:
+#        if key in my_dict:
+#            value is replaced
+#        if key not in my_dict:
+#            create value
+#    return (my_dict)
