e92022d01319de63cdd055ae73db53ebebaf4c85
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-13 07:06:32 +0000

    Pseudocode

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(my_dict, key, value):
+    for key, value in my_dict:
+        if key in my_dict:
+            value is replaced
+        if key not in my_dict:
+            create value
+    return (my_dict)
