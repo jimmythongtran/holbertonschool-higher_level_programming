775afa00d6fc68aa3c16c58e4a0fd1e79dbac592
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-13 07:24:05 +0000

    Output now

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(my_dict, key=""):
-    if my_dict == {} or my_dict is None:
-        return None
     if key in my_dict:
         del my_dict[key]
-    return my_dict
+    return (my_dict)
