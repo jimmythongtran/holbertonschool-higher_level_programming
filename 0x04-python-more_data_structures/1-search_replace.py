52f3ab94cf823a3a6f3bb785ed955b801cf79233
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-13 00:03:10 +0000

    Back to loop

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    #  makes copy
+    new_list = my_list[:]
     for i in range(0, len(my_list)):
-        if i == search:
-            my_list[search] = replace
-        return (my_list)
+        if new_list[i] == search:
+            new_list.pop(new_list[i])
+            new_list.insert(i, replace)
+    return (new_list)
+#        if i == search:
+#            my_list[search] = replace
+#        return (my_list)
