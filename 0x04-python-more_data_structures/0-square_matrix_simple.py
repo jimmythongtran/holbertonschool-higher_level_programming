582a842862b09050cbf6e6024c5e029db35eb1c8
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-13 01:32:17 +0000

    No output

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-def square_matrix(matrix=[]):
-    for row in range(0, len(matrix)):
-        for i in range(0, len(matrix[row])):
-           i = (i * i)
-           i = (i **.5)
+def square(num):
+    return num * num
+
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for i in range(0, len(matrix)):
+        new_list.append(list(square(num **.5)))
+    return (new_list)
+    
+#    for row in range(0, len(matrix)):
+#        for i in range(0, len(matrix[row])):
+#           i = (i * i **.5)
 #    return matrix
