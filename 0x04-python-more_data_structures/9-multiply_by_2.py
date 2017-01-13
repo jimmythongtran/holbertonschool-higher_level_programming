56348711cb653dbc5d6eb4bb38e77c1ad4293bbf
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-13 07:36:58 +0000

    Only multiplies last

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def multiply_by_2(my_dict):
-    my_dict = new_dict
-    # multiply the values by 2
+    new_dict = dict(my_dict)
+    for i in new_dict:
+        new_dict[i] *= 2
+        return (new_dict)
+#    my_dict = new_dict
+# multiply the values by 2
 #    multiplied = new_dict, key=new_dict.get * 2
-    return (multiplied)
+#    return (multiplied)
