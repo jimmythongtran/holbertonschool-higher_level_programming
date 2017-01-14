5df02793d1c1e81af3b3266b6067df724b2984d4
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-14 03:53:08 +0000

    Love list comps

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(0, len(my_list)):
-        if new_list[i] == search:
-            new_list.pop(new_list[i])
-            new_list.insert(i, replace)
-    return (new_list)
+    return [replace if element == search else element for element in my_list]
+#    new_list = my_list[:]
+#    for i in range(0, len(my_list)):
+#        if new_list[i] == search:
+#            new_list.pop(new_list[i])
+#            new_list.insert(i, replace)
+#    return (new_list)
