4aefc043187fdac9ada095f353a118e1b61a3570
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-14 02:49:41 +0000

    Zeroes missing

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    try:
-        new_list = []
-        for i in range(0, len(my_list1)):
-            new_list.append(my_list1[i] / my_list2[i])
+    new_list = []
+    i = 0
+    while i < list_length:
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
 #        list_length = [my_list_1i/my_list_2i for my_list1i,my_list2i in zip(my_list1,my_list2)]
-    except TypeError:
-        print("wrong type")
-    except ZeroDivisionError:
-        print("division by 0")
-    except IndexError:
-        print("out of range")
-    finally:
-        return (new_list) # with all divisions
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+           i += 1
+    return (new_list) # with all divisions
