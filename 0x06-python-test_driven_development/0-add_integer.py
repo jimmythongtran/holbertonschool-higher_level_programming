91d732ac144ca1739bc63255f3779dd7c74dc75c
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-17 20:47:02 +0000

    First draft

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def add(a, b):
+    """Return the sum of a and b.
+    >>> add(1, 2)
+    3
+    >>> add(100, -2)
+    98
+    >>> add(100.3, -2)
+    98
+    """
+    return (int(a + b))
+
+if __name__ == '__main__':
+    from doctest
+    doctest.testmod()
