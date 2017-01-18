0af544632cd13a514fac59911074a990caf331c7
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-18 03:09:25 +0000

    Added docstrings

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+This is the add_integer function
+that takes in two variables and returns their sum
+Note: value can only be integers, not float or otherwise
+"""
 
 def add_integer(a, b):
+    """
+    returns the sum of two integer variables
+    """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
