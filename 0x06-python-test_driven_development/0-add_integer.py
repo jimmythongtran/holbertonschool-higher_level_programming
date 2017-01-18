d4c4c7ec5026707c6a616262702f9f7375bbc44e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-18 00:59:16 +0000

    Rewrote with isinstance but fails all 9 tests

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def add_integer(a, b):
-    if type(a) != int and type(a) != float:
+    if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
     return int(a + b)
 
