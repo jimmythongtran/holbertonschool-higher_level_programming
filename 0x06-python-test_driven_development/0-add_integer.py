d6d8c4421b8570e7a48c191f3e916f802e0d754c
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-18 00:02:17 +0000

    Output but not desired test

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    """Return the sum of a and b.
-    >>> add(1, 2)
-    3
-    >>> add(100, -2)
-    98
-    >>> add(100.3, -2)
-    98
-    """
-    return (int(a + b))
+def add_integer(a, b):
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    return int(a + b)
 
 if __name__ == '__main__':
-    from doctest
-    doctest.testmod()
+    import doctest
+    doctest.testfile(0-add_integer.txt)
