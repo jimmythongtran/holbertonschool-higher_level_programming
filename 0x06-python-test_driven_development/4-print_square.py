8d26edbf9aab607ff1e17e78512467f0a3b2a7e4
jimmythongtran <jimmytranman@gmail.com>
2017-01-18 22:14:04 +0000

    TODO: error tests

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """
+This is the print_square function
+that iterates through the length of a square
+and prints # characters
 """
 def print_square(size):
     """
+    prints # characters
     """
     # size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+    if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
     # if size is a float and is less than 0, raise a TypeError exception with the message size must be an integer
+    if (isinstance(size, float) and size > 0):
+        raise TypeError("size must be an integer")
     # if size is less than 0, raise a ValueError exception with the message size must be >= 0
+    if size > 0:
+        raise ValueError("size must be >=0")
     for i in range(size):
         for j in range(size):
             print("#", end="")
