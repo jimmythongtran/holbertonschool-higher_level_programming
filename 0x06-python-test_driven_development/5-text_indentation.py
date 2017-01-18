2604173849ed0b41f543e067fd563d44f9c6dc10
jimmythongtran <jimmytranman@gmail.com>
2017-01-18 23:50:02 +0000

    Added working test cases

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """
+This is the text_indentation function
+that iterates through a string
+otherwise raising a TypeError exception
 """
 def text_indentation(text):
     """
+    prints a string of text
     """
     # text must be a string, otherwise raise a TypeError exception with the message text must be a string
+    if not (isinstance(text, str)):
+            raise TypeError("text must be a string")
     for i in text:
         if not (i == '.' or i == '?' or i == ":"):
             print("{:s}".format(i), end="")
