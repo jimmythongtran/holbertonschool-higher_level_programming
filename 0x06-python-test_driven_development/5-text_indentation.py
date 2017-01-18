3e4bde25e91577bcb49ac470c830cd290c264503
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-18 05:05:21 +0000

    TODO errors and docstring

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+"""
+def text_indentation(text):
+    """
+    """
+    # text must be a string, otherwise raise a TypeError exception with the message text must be a string
+    for i in text:
+        if not (i == '.' or i == '?' or i == ":"):
+            print("{:s}".format(i), end="")
+        else:
+            print("{:s}\n".format(i))
+
+if __name__ == '__main__':
+        doctest.testfile(5-text_indentation.txt)
