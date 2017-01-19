6d656e139d49b73088c592ef1f3fd3dbafa4b16e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-19 21:56:31 +0000

    This works

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(my_dict):
-    new_dict = dict(my_dict)
-    for i in new_dict:
-        new_dict[i] *= 2
-        return (new_dict)
-#    my_dict = new_dict
-# multiply the values by 2
-#    multiplied = new_dict, key=new_dict.get * 2
-#    return (multiplied)
+    new_dict = {}
+    for key in my_dict.keys():
+        new_dict[key] = (my_dict[key] * 2)
+    return (new_dict)
