42bf0c7ce369c3457875df2f26a9453df456a038
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-19 06:07:32 +0000

    pep8

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -8,13 +8,10 @@ def print_square(size):
     """
     prints # characters
     """
-    # size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
     if not (isinstance(size, int)):
             raise TypeError("size must be an integer")
-    # if size is a float and is less than 0, raise a TypeError exception with the message size must be an integer
     if (isinstance(size, float) and size > 0):
         raise TypeError("size must be an integer")
-    # if size is less than 0, raise a ValueError exception with the message size must be >= 0
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
