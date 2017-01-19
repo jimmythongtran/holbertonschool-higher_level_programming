de01d6e67701ee61667fa31671cba68a81dbf243
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-19 22:09:54 +0000

    Append to a new list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    new_list = []
     for unique in my_list:
-        if my_list is None:
-            return None
-        unique = set(my_list)
-        return(sum(unique))
+        if unique not in new_list:
+            new_list.append(unique)
+    return(sum(new_list))
+#    for unique in my_list:
+#        if my_list is None:
+#            return None
+#        unique = set(my_list)
+#        return(sum(unique))
