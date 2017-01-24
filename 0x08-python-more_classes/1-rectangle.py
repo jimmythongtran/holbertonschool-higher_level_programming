f7e33d2ad2cf26d289de18f47cee3312ec3a6a32
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 22:03:27 +0000

    Changed init & int(value)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,7 +13,15 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
- 
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if int(width) < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if int(height) < 0:
+            raise ValueError("height must be >= 0")
+
     @property
     def width(self):
         return self.__width
@@ -22,7 +30,7 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if width < 0:
+        if int(value) < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -34,6 +42,6 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        if int(value) < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
