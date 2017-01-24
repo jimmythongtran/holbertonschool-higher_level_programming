2e1d32d08ec43f361a3e13dbb25192718d27a497
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 05:18:21 +0000

    Changed to type

--- 0x07-python-classes/3-square.py
+++ 0x07-python-classes/3-square.py
@@ -11,7 +11,8 @@ class Square:
     This is a square class
     """
     def __init__(self, size=0):
-        if not (isinstance(size, int)):
+#        if not (isinstance(size, int)):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
