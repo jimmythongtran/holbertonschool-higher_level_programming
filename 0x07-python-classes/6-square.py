e17c758a6b854639b712f4d8b34a35cd647ce2cb
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 05:32:39 +0000

    First commit

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -1,4 +1,37 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+This is the square module
+
+This is a Square class that calculates area based on size
+"""
+
+
+class Square:
+    """
+    This is a square class
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.size == 0:
+            print(" ")
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
