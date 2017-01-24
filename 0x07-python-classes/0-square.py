fb873861f63a12b42d1b0a809f73b820f5da1bcc
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 01:48:52 +0000

    Pep8

--- 0x07-python-classes/0-square.py
+++ 0x07-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-""" This is a Square class"""
+"""
+This is the Square module.
+
+This is a Square class inside the square module.
+"""
+
 
 class Square:
+    """
+    This is the square class
+    """
     pass
--- 0x07-python-classes/1-square.py
+++ 0x07-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+This is the square module
+
+This is a Square class defined by size
+"""
+
+
+class Square:
+    """
+    This is the square class
+    """
+    def __init__(self, size):
+        self.__size = size
