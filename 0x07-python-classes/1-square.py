f048e4954ab7852fe37d0f9afbf39f61ba5c4df0
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 05:46:08 +0000

    Amended comment

--- 0x07-python-classes/1-square.py
+++ 0x07-python-classes/1-square.py
@@ -8,7 +8,7 @@ This is a Square class defined by size
 
 class Square:
     """
-    __init__ creates space in memory
+    Reserved word __init__ creates space in memory
     """
     def __init__(self, size):
         self.__size = size
