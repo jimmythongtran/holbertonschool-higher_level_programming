1329805f547f79b18e4466ab5cb647fb1b2e834c
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 02:05:58 +0000

    Rethinking

--- 0x07-python-classes/3-square.py
+++ 0x07-python-classes/3-square.py
@@ -10,10 +10,12 @@ class Square:
     """
     This is a square class
     """
-    def __init__(self, size=0, area):
+    def __init__(self, size=0):
         if not (isinstance(size, int)):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        self.__area = area
+
+    def area(self, area):
+        self.__area = (self.__area * self.__area)
