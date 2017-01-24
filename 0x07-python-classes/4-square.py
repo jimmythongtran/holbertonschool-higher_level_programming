663dbd0ac9c21686dd38c49fe2809af9eff86089
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 05:23:23 +0000

    Removed isinstance

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -19,11 +19,11 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not (isinstance(value, int)):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        return (self.size * self.size)
+        return (self.__size * self.__size)
