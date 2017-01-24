f999e4735cfd5d2611ef62ec9fb9252892900924
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 05:17:10 +0000

    Changed to type

--- 0x07-python-classes/2-square.py
+++ 0x07-python-classes/2-square.py
@@ -11,8 +11,9 @@ class Square:
     This is a square class
     """
     def __init__(self, size=0):
-        if not (isinstance(size, int)):
+#        if not (isinstance(size, int)):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
