127a93cb5f8e9ce84f8d526ac857123f04952965
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-24 02:20:51 +0000

    Returned it

--- 0x07-python-classes/3-square.py
+++ 0x07-python-classes/3-square.py
@@ -17,5 +17,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self, area):
-        self.__area = (self.__area * self.__area)
+    def area(self):
+        return (self.__size * self.__size)
