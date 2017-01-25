699548263647b534d9dcd9f5294f30db6c8f614c
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-25 19:20:10 +0000

    Changed to join

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -13,14 +13,6 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if int(width) < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if int(height) < 0:
-            raise ValueError("height must be >= 0")
 
     @property
     def width(self):
@@ -55,15 +47,9 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        string = ""
-        if self.__width == 0 or self.__height == 0:
-            return string
-        for i in range(self.__width):
-            for j in range(self.__height):
-                    string += "#"
-            string += '\n'
-        string = string[:-1]
-        return (string)
+        if self.__width is 0 or self.__height == 0:
+            return ("")
+        return "\n".join(("#" * self.__width) for i in range(self.__height))
 
     def __repr__(self):
         width = str(self.__width)
