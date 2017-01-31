6c9c9bdff6f10749ceb40d25b0afdd6385a3033a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-31 00:12:32 +0000

    pep8

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,4 +1,33 @@
 #!/usr/bin/python3
-def lookup(obj):
-    return (0)
-print(lookup(obj))
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+#    def area(self):
+#        return self.__size * self.__size
