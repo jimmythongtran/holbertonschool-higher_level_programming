f4fb817fd225a62d67899d064200bf6a6c9c0e0e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-01-31 00:19:33 +0000

    yay

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
-    return (0)
-print(is_same_class(obj, a_class))
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def __str__(self):
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
