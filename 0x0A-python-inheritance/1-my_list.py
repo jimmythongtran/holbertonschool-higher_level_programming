ded14d4a31a1bbc196c980390cecd5a456af95f0
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-02 03:39:53 +0000

    Added comments

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+This module contains one class
+https://github.com/anne75 developed the test
+run it with python3 -m doctest ./tests/1-my_list.txt
+"""
 class MyList(list):
+    """
+    Inherits from the list class
+    Assumes all elements in list are ints
+    """
     def print_sorted(self):
-        pass
-        print(sorted(self))
-        pass
+        print("{:d}".format(sorted(self)))
