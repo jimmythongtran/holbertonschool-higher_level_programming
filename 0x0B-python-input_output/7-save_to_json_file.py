4a02c69bed9ed70c8b393e3d3c4f1af7cee980ff
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-03 05:37:03 +0000

    Syntax understood

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def save_to_json_file(my_obj, filemame):
-    return (0)
-print(save_to_json_file(my_obj, filemame))
+def save_to_json_file(my_obj, filename):
+    import json
+    with open(filename, mode='w', encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
