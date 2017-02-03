0f3e462c29312f2698a047135e811a44223cf9e5
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-03 21:26:34 +0000

    imported argv

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 import json
-import sys
+from sys import argv
 import os.path
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-
+# saving list as JSON representation
+filename = "add_item.json"
+# check if file exists
+if os.path.isfile(filename):
+    myList = load_from_json_file(filename)
+# if file doesn't exist, create it
+else:
+    myList = []
+count = 0
+# loop through argv not counting first arg
+for i in argv:
+    if count != 0:
+        myList.append(i)
+    count += 1
+save_to_json_file(myList, filename)
