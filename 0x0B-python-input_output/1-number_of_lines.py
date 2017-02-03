33dc61849b596b670e11515cd8a39731b66e37ab
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-03 02:26:57 +0000

    Cool

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def number_of_lines(filename=REPLACEquot;REPLACEquot;):
-    return (0)
-print(def number_of_lines(filename=REPLACEquot;REPLACEquot;))
+def number_of_lines(filename=""):
+    with open(filename, encoding="UTF8") as f:
+        count_line = 0
+        for lines in f:
+            count_line += 1
+        return (count_line)
