4ab44ba6a391fc7ef3667a8d56cf8976d564bcff
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-28 21:27:13 +0000

    Error in MySQL syntax

---
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script lists all priveleges of the MySQL users user_0d_1 and user_0d_2 on your server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVELEGES ON * . * TO 'user_0d_1'@'localhost';
