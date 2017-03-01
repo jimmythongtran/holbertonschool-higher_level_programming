12819f84c2916ba19eb233e0e21d81aa6fbb5b9a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-01 00:44:39 +0000

    Removed multiple NOT NULL, changed ID order

---
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- This script creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+state_id INT, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+name VARCHAR(256) NOT NULL
+); 
