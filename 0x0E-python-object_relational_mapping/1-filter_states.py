f6af4e6a36d135e0fb869bc873305f2c9839729c
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-08 21:59:01 +0000

    name like

--- 0x0E-python-object_relational_mapping/1-filter_states.py
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host="localhost", port=3306,
+                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+
+for info in cur.fetchall():
+    print(info)
+
+cur.close()
+db.close()
