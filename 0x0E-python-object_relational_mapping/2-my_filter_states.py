3815a9ddb836671f1b8a4081c57d5b192d340940
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-08 22:46:49 +0000

    Fixed line length, missed end bracket

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+'''
+This is the '2-filter_states' module
+
+2-filter_states connects to the hbtn_0e_0_usa database and
+retrieves the name when specified by the argument passed through
+'''
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host="localhost", port=3306,
+                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+cur = db.cursor()
+cur.execute("SELECT * FROM states\
+        WHERE name = '{:s}' ORDER BY states.id ASC".format(sys.argv[4]))
+
+for info in cur.fetchall():
+    print(info)
+
+cur.close()
+db.close()
