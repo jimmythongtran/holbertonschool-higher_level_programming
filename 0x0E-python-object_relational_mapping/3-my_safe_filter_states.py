763661df3f96cbb621aa983672a0dd1ed4164a2d
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-09 02:02:54 +0000

    Test

--- 0x0E-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0E-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,26 +5,20 @@ This is the '3-my_safe_filter_states' module
 3-my_safe_filter_states connects retrieves the name when specified by
 the argument passed through that's safe from MySQL injection!
 '''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-import MySQLdb
-import sys
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+        WHERE name = '{:s}' ORDER BY\
+        states.id ASC".format(sys.argv[4].split";"))
 
-cur = db.cursor()
+    for info in cur.fetchall():
+        print(info)
 
-# cuts rest of query off so TRUNCATE TABLE never queries
-for char in sys.argv[4]:
-    if char is ';':
-        break
-    sys.argv += char
-
-cur.execute("SELECT * FROM states\
-        WHERE name = '{:s}' ORDER BY states.id ASC".format(sys.argv[4]))
-
-for info in cur.fetchall():
-    print(info)
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()
