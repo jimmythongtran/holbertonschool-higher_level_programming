03f3f0bd7a58e8fad9b6fc0539c4d1d2eb49b484
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-09 22:47:47 +0000

    Pep8 .first()

--- 0x0E-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0E-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+'''
+This is the '8-model_state_fetch_first' module
+
+This prints the first State object from the database hbtn_0e_6_usa
+'''
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    entry = session.query(State).first()
+    if entry:
+        print("{}: {}".format(entry.id, entry.name))
+    else:
+        print("Nothing")
