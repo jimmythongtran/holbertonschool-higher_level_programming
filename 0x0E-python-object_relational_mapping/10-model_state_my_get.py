e3bdd18a46c48d6dc0202abcb912f0657d0a40be
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-09 23:28:29 +0000

    Displays states.id not Not Found

--- 0x0E-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0E-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+'''
+This is the '10-model_state_my_get' module
+
+This prints the State object with the name passed as argument from the database
+'''
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+ 
+    for entry in session.query(State).filter(State.name == sys.argv[4]):
+        print("{}".format(entry.id))
