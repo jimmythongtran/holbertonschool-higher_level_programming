9d3b46c328c0ce9369596164278be24843a9b94a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-09 00:52:04 +0000

    if name thing

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -5,19 +5,19 @@ This is the '2-filter_states' module
 2-filter_states connects to the hbtn_0e_0_usa database and
 retrieves the name when specified by the argument passed through
 '''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-import MySQLdb
-import sys
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-cur = db.cursor()
-cur.execute("SELECT * FROM states\
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
         WHERE name = '{:s}' ORDER BY states.id ASC".format(sys.argv[4]))
 
-for info in cur.fetchall():
-    print(info)
+    for info in cur.fetchall():
+        print(info)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
