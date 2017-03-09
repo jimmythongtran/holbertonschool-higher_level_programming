7128c5e1b5b02ec7fe9ee2be5bd75f5d0d2dfa28
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-09 00:32:18 +0000

    Your code should not be executed when imported

--- 0x0E-python-object_relational_mapping/1-filter_states.py
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -5,18 +5,19 @@ This is the '1-filter_states' module
 1-filter_states connects to the hbtn_0e_0_usa database and
 retrieves state names beginning with 'N' via Python script
 '''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-import MySQLdb
-import sys
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+            WHERE name LIKE 'N%' ORDER BY states.id ASC")
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    for info in cur.fetchall():
+        print(info)
 
-for info in cur.fetchall():
-    print(info)
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()
