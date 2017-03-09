1111c3d83526c2d822845a4217b06bcd15574dcc
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-09 00:12:20 +0000

    Reduced docstring, tripple comment query

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-'''
-This is the '0-select_states' module
-
+'''This is the '0-select_states' module
 0-select_states connects to the hbtn_0e_0_usa database and
 retrieves a specified MySQL query via Python script
-
 '''
 import MySQLdb
 import sys
@@ -13,7 +10,7 @@ db = MySQLdb.connect(host="localhost", port=3306,
                      user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
 cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
 
 for info in cur.fetchall():
     print(info)
