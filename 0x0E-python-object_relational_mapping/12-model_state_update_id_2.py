9d9ac0ce4a35b45bbac9220bfd5f9227536571ca
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-10 04:27:11 +0000

    Clean pep8

--- 0x0E-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0E-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+'''
+This is the '12-model_state_update_id_2' module
+
+This writes a script changes the name of a State object from database
+'''
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for entryUpdate in session.query(State).filter(State.id == 2):
+        entryUpdate.name = 'New Mexico'
+        session.commit()
