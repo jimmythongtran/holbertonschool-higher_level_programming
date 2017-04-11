723a8817a7f4e0dda332e7e5d58a9e08f7d662bf
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-11 22:08:37 +0000

    Fixed Python3 format

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 """
 this script fetches https://intranet.hbtn.io/status using urllib
 """
-if __name__ == "__main__":
-    import urllib.request
+import urllib.request
 
+if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-    print("Body response:")
-    print("    - type:", type(html))
-    print("    - content:", html)
-    print("    - utf8 content:", html.decode("utf8"))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf8")))
