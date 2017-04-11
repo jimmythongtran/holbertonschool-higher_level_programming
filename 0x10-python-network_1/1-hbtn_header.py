67fd8e6fe98216bf20bfe984732a5394651da1bc
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-11 22:35:10 +0000

    Pep8 check

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+this script takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of response
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print("{}".format(html.get('X-Request-Id')))
