3d3e59cf17a667965135c613bc881fdc0f174036
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-11 23:40:21 +0000

    Destroyed container, works now

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+this takes in a URL and email, sends a POST request to passed URL with email
+as parameter, and displays response body (decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    with urllib.request.urlopen(url, data) as response:
+        html = response.read()
+        print(html.decode(encoding='UTF-8'))
