86745546cd3dc8e7a67a44c9554910d8bacbc9b6
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 18:50:32 +0000

    Works

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+this takes in github credentials (un and pw) and uses the
+Github API to display ID
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = requests.get("https://api.github.com/user", auth=(user, password))
+    print(r.json().get("id"))
