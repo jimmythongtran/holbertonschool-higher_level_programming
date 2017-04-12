28f29725488c0b0f51334deb03460d209133a0e3
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 22:16:29 +0000

    LSD haha

--- 0x0F-python-network_0/41-post_params.sh
+++ 0x0F-python-network_0/41-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# this takes in a URL, sends a POST request to passed URL, and displays response body
+curl -sL -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
