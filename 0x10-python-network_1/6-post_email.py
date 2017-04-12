38be29cee12a4a88930dadd5eb64ee6728c3c53d
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 02:04:00 +0000

    Forgot to pep8

--- 0x10-python-network_1/6-post_email.py
+++ 0x10-python-network_1/6-post_email.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+this takes in a URL and email, sends a POST request to
+passed URL with email as parameter, lastly displays
+response body
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
