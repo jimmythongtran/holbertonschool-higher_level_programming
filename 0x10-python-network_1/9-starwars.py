0ed97a5d80d8f2ec23be00c7ce58e30821f6a7e7
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 17:24:46 +0000

    print(r.text)

--- 0x10-python-network_1/9-starwars.py
+++ 0x10-python-network_1/9-starwars.py
@@ -10,7 +10,9 @@ import sys
 if __name__ == "__main__":
     url = "https://swapi.co/api/people/?search=" + sys.argv[1]
     r = requests.get(url)
-    search = r.json()["search"]
-    print("Number of result: {}".format(len(search)))
+    # print(r.text)
+    count = r.json()["count"]
+    search = r.json()["results"]
+    print("Number of result: {}".format(count))
     for i in search:
         print(i["name"])
