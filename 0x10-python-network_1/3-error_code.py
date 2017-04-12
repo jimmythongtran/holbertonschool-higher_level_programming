32ebbdef6462a690a867125df0dce284ae533c84
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 00:48:06 +0000

    Finally

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+this takes in a URL, sends a request to URL, displays
+response body (decoded in utf-8)
+"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
