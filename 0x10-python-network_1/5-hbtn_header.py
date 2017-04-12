b5e8399445e8f22d0d37f55185cd9af56087fe59
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 01:54:21 +0000

    One more try for one check

--- 0x10-python-network_1/5-hbtn_header.py
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -9,6 +9,5 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    r = requests.get(url)
-    print(r.headers['x-request-id'])
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
