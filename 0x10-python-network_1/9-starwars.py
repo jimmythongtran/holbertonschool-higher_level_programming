76ad0867a6f3f92a13342782ff9edb206040d4d4
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 04:37:55 +0000

    doesn't work starrrr warrrrss

--- 0x10-python-network_1/9-starwars.py
+++ 0x10-python-network_1/9-starwars.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+this takes in a string and sends a search request to
+the Star Wars API
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people/?search=" + sys.argv[1]
+    r = requests.get(url)
+    search = r.json()["search"]
+    print("Number of result: {}".format(len(search)))
+    for i in search:
+        print(i["name"])
