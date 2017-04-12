d88482ec17e4421bbe8b8e65a6ae85c8b7809bf8
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 19:37:26 +0000

    dictionary format

--- 0x10-python-network_1/8-json_api.py
+++ 0x10-python-network_1/8-json_api.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+this takes in a letter and sends in a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or not sys.argv[1].isalpha():
+        print("No result")
+    else:
+        q = sys.argv[1]
+        url = "http://0.0.0.0:5000/search_user"
+        r = requests.post(url, data={'q': q})
+        theId = r.json().get("id")
+        name = r.json().get("name")
+        print("[{}] {}".format(theId, name))
