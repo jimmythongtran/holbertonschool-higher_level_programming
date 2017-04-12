06b2f4cd81a330de3d721826c1ba53fe017729a8
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 04:03:18 +0000

    http://0.0.0.0:5000/doesnt_exist wrong output

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+this takes in a URL and email, sends a POST request to
+passed URL with email as parameter, lastly displays
+response body
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    try:
+        print(r.text)
+    except Exception as e:
+        print("Error code: {}".format(r.status_code))
