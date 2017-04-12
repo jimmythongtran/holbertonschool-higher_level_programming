3c0028c9d6fa8c56feef5cbc823390a690e5d9dd
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-12 04:46:55 +0000

    thank you automateboringstuff

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -11,6 +11,7 @@ import sys
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     try:
+        r.raise_for_status()
         print(r.text)
     except Exception as e:
         print("Error code: {}".format(r.status_code))
