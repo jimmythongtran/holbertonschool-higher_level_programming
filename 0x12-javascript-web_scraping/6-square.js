a4913480544280d53dd08e60d71d4135bad56206
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-05-01 02:07:30 +0000

    Semistandard --fix don't work

--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const SquareX = require('./5-square').Square;
+
+function Square (size) {
+  SquareX.call(this, size);
+  this.size = size;
+
+  Square.prototype.charPrint = function (c) {
+	  if (c === undefined) {
+		  c = 'X';
+	  }
+
+	  for (let i = 0; i < this.size; i++) {
+		  console.log(c.repeat(this.size));
+	  }
+  };
+}
+
+exports.Square = Square;
