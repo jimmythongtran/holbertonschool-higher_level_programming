97f41f9c0d9a9920f8d4962e2106b87a564326fc
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-05-01 02:27:11 +0000

    trim()

--- 0x12-javascript-web_scraping/7-readme.js
+++ 0x12-javascript-web_scraping/7-readme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (error, info) => {
+  if (error) {
+    return console.log(error);
+  } else {
+    console.log(info.trim());
+  }
+});
