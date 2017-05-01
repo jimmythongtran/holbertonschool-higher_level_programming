63076ccb8bed801e892471d6ea06a948acfe13d2
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-05-01 00:30:43 +0000

    finally workS

--- 0x12-javascript-web_scraping/10-starwars_title.js
+++ 0x12-javascript-web_scraping/10-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + parseInt(process.argv[2]);
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
