2ade00dc10cec5c590a2a3fa3bba964d316590a3
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-05-01 01:47:17 +0000

    Semistandard --fix doesn't work

--- 0x12-javascript-web_scraping/4-rectangle.js
+++ 0x12-javascript-web_scraping/4-rectangle.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+function Rectangle (w, h) {
+  if (w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
+  }
+
+  this.print = function () {
+	  for (let i = 0; i < this.height; i++) {
+		  console.log('X'.repeat(this.width));
+	  }
+  };
+
+  this.rotate = function () {
+	  var temp = this.width;
+	  this.width = this.height;
+	  this.height = temp;
+  };
+
+  this.double = function () {
+	  this.width *= 2;
+	  this.height *= 2;
+  };
+}
+exports.Rectangle = Rectangle;
