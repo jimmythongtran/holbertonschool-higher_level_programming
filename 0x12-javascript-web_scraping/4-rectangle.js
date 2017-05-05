75c74c7bca2ad8c3af81db43b8a6b7ba2289595a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-05-05 01:31:11 +0000

    fixed

--- 0x12-javascript-web_scraping/4-rectangle.js
+++ 0x12-javascript-web_scraping/4-rectangle.js
@@ -7,20 +7,20 @@ function Rectangle (w, h) {
   }
 
   this.print = function () {
-	  for (let i = 0; i < this.height; i++) {
-		  console.log('X'.repeat(this.width));
-	  }
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   };
 
   this.rotate = function () {
-	  var temp = this.width;
-	  this.width = this.height;
-	  this.height = temp;
+    var temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   };
 
   this.double = function () {
-	  this.width *= 2;
-	  this.height *= 2;
+    this.width *= 2;
+    this.height *= 2;
   };
 }
 exports.Rectangle = Rectangle;
